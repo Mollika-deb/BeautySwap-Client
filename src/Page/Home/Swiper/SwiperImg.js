1c77d3a99228de446a5ef63c4b6e3fb4b689a3c4
Mollika-Deb <mollika1551@gmail.com>
2023-09-12 22:30:31 +0600

    swiper update

--- src/Page/Home/Swiper/SwiperImg.js
+++ src/Page/Home/Swiper/SwiperImg.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { FreeMode } from 'swiper';
+import { Autoplay, FreeMode } from 'swiper';
 import 'swiper/css'
 import 'swiper/css/free-mode'
 import { EffectCoverflow, Pagination, Navigation } from 'swiper';
@@ -17,34 +17,37 @@ import slide_image_7 from '../../../images/makeup7.jpg';
 
 const SwiperImg = () => {
     return (
-        <div className=' mx-10 items-center justify-center my-40  sliderImg' >
+        <div className='w-[90vw] md:w-[90vw] lg:w-[80vw] mx-auto items-center justify-center my-40  sliderImg' >
             <div>
-                <h1 className='text-5xl text-center ' style={{ fontFamily: 'Libre Baskerville', color:'#965a3e' }}>Follow Our Trending Items</h1>
-                <h1 className='text-center text-3xl text-gray-700 font-semibold py-5'>Buy And Get Discount</h1>
+                <h1 className='text-3xl md:text-3xl lg:text-5xl text-center ' style={{ fontFamily: 'Libre Baskerville', color:'#965a3e' }}>Follow Our Trending Items</h1>
+                <h1 className='text-center text-xl md:text-xl lg:text-3xl text-gray-700 font-semibold pt-3 pb-6'>Buy And Get Discount</h1>
             </div>
             <Swiper
-                modules={[Navigation, Pagination]}
-                spaceBetween={20}
-                slidesPerView={3}
-                loop={true} // Set loop to true
-                navigation
-                pagination={{ clickable: true }}
-                scrollbar={{ draggable: true }}
-                onSwiper={(swiper) => console.log(swiper)}
-                onSlideChange={() => console.log('slide change')}
+//                
 
-                breakpoints={{
-                    // when window width is >= 640px
-                    640: {
-                      display: 'none'
-                    },
-                    // when window width is >= 768px
-                    768: {
-                      slidesPerView: 3,
-                      spaceBetween: 40,
-                    },
-                }
-            }
+slidesPerView={1}
+spaceBetween={10}
+centeredSlides={true}
+autoplay={{
+  delay: 2000,
+  disableOnInteraction: false,
+}}
+loop={true}
+breakpoints={{
+  640: {
+    slidesPerView: 1,
+    spaceBetween: 10,
+  },
+  768: {
+    slidesPerView: 2,
+    spaceBetween: 10,
+  },
+  1024: {
+    slidesPerView: 4,
+    spaceBetween: 20,
+  },
+}}
+modules={[Autoplay, Pagination, Navigation]}
 
             >
 
@@ -71,6 +74,12 @@ const SwiperImg = () => {
                     </SwiperSlide>
                     <SwiperSlide>
                         <img  src={slide_image_7} alt="slide_image" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img  src={slide_image_5} alt="slide_image" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img  src={slide_image_6} alt="slide_image" />
                     </SwiperSlide>
              
 
